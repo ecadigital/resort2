dc52a181a8b43481502606b7353d62e2f9ec3f4e
tuckkiez <i3adtzmaru@gmail.com>
2014-10-22 15:37:55 +0700

    offer
    offer

---
+++ offer-packages.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=1167">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/layout.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+        
+        <?php include("inc-topmenu.php");?>
+        
+        
+        <div id="inner-wrapper">
+	        <h1 class="left">Special Occasions</h1>
+	 
+        
+        
+    
+	    	<div id="special-wrapper">
+	    		<div class="blog">
+	    			<div class="blog-in">
+	    			<span>We cater for special occasions<br>
+	    			 such as Birthday Party,<br>
+					 days for guests and family. <br> </span>
+					 <div class="white-line"></div>
+					 <span>Please do contact us <br>
+					 for more information at<br> : cm@laprovenceSP.com</span>
+	    			</div>
+	    		</div>
+	    	</div>
+	    	
+	    	<div id="sp-img1" class="overflow"><img src="img/sp-img1.jpg">
+	    	</div>
+	    	<div id="sp-img2" class="overflow"><img src="img/sp-img2.jpg">
+	    	</div>
+                 <div class="clearfix"></div>
+           
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                <div class="clearfix"></div>
+
+             <h1 class="left fleft">Special Occasions</h1>
+            <div id="sp-img3" class="overflow"><img src="img/sp-img3.jpg">
+            </div>
+            <div id="sp2-left">
+                <div id="sp-img4" class="overflow"><img src="img/sp-img4.jpg">
+                </div>
+                <div id="sp-img5" class="overflow"><img src="img/sp-img5.jpg">
+                </div>
+                <div id="sp-img6" class="overflow"><img src="img/sp-img6.jpg">
+                </div>
+            </div>
+            
+            <div id="sp2-right">
+                <div id="blog2">
+                    <div id="blog-in2">
+                        <span>
+                            A one night Wedding Package <br>
+                            of THB 300,000.- <br>
+                             <div class="white-line"></div>
+                            Inclusive of ;<br>
+                            <ul>
+                            <li>Superior and Deluxe Villas <br>
+                                with 3 persons sharing per room</li>
+                            <li>One Two-bedroom Villa <br>
+                                for 5 persons sharing in room</li>
+                            <li>Breakfast for all guests in-house <br>
+                                on the following day <br>
+                                (counting at 50 persons)</li>
+                            <li>A buffet for 80 to 100 guests <br>
+                                inside dining hall   <br>   
+                                with soft drinks for 3 hours.</li>
+                            <li>A flower decoration as per La Provence <br>
+                                (to be discussed with guests directly)</li>
+                            <li>A complimentary stay for Bride & Groom <br>
+                                one night prior to the event </li>
+                            </ul>    
+                                 <div class="white-line"></div>
+                                 Please contact us for more information <br>
+                                at : cm@laprovenceSP.com
+                             </span>
+
+                    </div>
+                </div>
+            </div>
+                <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                <div class="clearfix"></div>
+             <h1 class="left fleft">Pre-wedding photo shooting package</h1><h2 class="h2small h2right">*Monday to Friday only*</h2>
+            <div id="pw-left">
+                <div id="blog3">
+                    <div id="blog-in3">
+                        <span>PACKAGE I  at 4,000 Baht <br>
+                        A special price when guest buy one<br>
+                        night accommodation through hotel<br>
+                        at normal rate.  Please call hotel <br>
+                        directly to request.<br><br>
+                        PACKAGE II  at 7,000 Baht<br>
+                        One day package<br> 
+                        with no accommodation<br>
+                         <div class="white-line"></div>
+                        Inclusive of;
+                        <ul class="smallli">
+                        <li>Photo shooting time starting <br>
+                            from 9 am to 7 pm<br> 
+                            (outdoor area only)</li>
+                        <li>Photo shooting at The Pool is<br>           
+                        allow till 4 pm. Only.</li>
+                        <li>Indoor photo shooting at Bride<br> 
+                             & Groom’s room.</li>
+                             </ul>
+                        Remark;
+                    
+                        <ul class="smallli">
+                        <li>Pets are not allow in the property.</li>
+                        <li>No outside food allow in hotel<br> 
+                            and restaurant area.</li>
+                        <li>Props, chairs and hotel belongings<br>      in rooms, restaurant and all  <br>           outdoor area can not be move from     <br>   original place.</li>  
+                        <li>A cash deposit of BHT 2,000 is to <br>      be place upon check-in to <br> 
+                            guaranteed for any damage in hotel<br>      property. Hotel will return if no <br>       damage occur upon check-out.</li>
+                            </ul>
+                            </span>
+                    </div>
+                </div>
+            </div>
+
+            <div id="pw-right">
+                <div id="pw-img1" class="overflow"><img src="img/pw-img1.jpg">
+                </div>
+                <div id="pw-img2" class="overflow"><img src="img/pw-img2.jpg">
+                </div>
+                <div id="pw-img3" class="overflow"><img src="img/pw-img3.jpg">
+                </div>
+                <div id="pw-img4" class="overflow"><img src="img/pw-img4.jpg">
+                </div>
+            </div>
+
+             <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                 <div class="clearfix"></div>
+            <div id="as-left">
+             <h1 class="left">Activities <br>
+                & Services</h1>
+                <h2 class="h2small">1.  Half-day trip to Huay Kok Moo 
+    plus Kayak<br><br>
+2.  ……..</h2>
+                </div>
+            <div id="as-right">
+               <div id="as-img1" class="overflow"><img src="img/as-img1.jpg">
+            </div>
+             <div id="as-img2" class="overflow"><img src="img/as-img2.jpg">
+            </div>
+        </div>
+
+                <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flower-line3.jpg">
+                </div>
+
+
+        <div class="gotop">
+	        <img src="images/gotop.png">
+        </div>
+        
+        </div>
+      <!--inner-wrapper-->
+        </div>
+       
+        
+        
+        
+<?php include('inc-footer.php');?> 
+        
+        
+        <script>
+	        $(document).ready(function(){
+		        $('#offera').addClass('active');
+		        $(this).areaAnimate0();
+	        });
+        </script>
+    </body>
+</html>
