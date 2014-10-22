5fb2fa32c3ec3bcab13f880e11334a3cdab98ec5
tuckkiez <i3adtzmaru@gmail.com>
2014-10-22 13:53:04 +0700

    Offer
    Offer

--- offer-packages.php
+++ offer-packages.php
@@ -29,38 +29,157 @@
         
         <div id="inner-wrapper">
 	        <h1 class="left">Special Occasions</h1>
-	     </div>
-        <!--inner-wrapper-->
+	 
         
-       
-       <div id="wrap-inner">
+        
+    
 	    	<div id="special-wrapper">
 	    		<div class="blog">
 	    			<div class="blog-in">
-	    			<p>We cater for special occasions<br>
+	    			<span>We cater for special occasions<br>
 	    			 such as Birthday Party,<br>
-					 Weddings or any given special<br>
-					 days for guests and family. <br> </p>
+					 days for guests and family. <br> </span>
 					 <div class="white-line"></div>
-					 <p>Please do contact us <br>
-					 for more information at<br> : cm@laprovenceSP.com</p>
+					 <span>Please do contact us <br>
+					 for more information at<br> : cm@laprovenceSP.com</span>
 	    			</div>
 	    		</div>
-	    		</div>
 	    	</div>
+	    	
 	    	<div id="sp-img1" class="overflow"><img src="img/sp-img1.jpg">
 	    	</div>
 	    	<div id="sp-img2" class="overflow"><img src="img/sp-img2.jpg">
 	    	</div>
+                 <div class="clearfix"></div>
+           
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                <div class="clearfix"></div>
 
-       
-               
-        
+             <h1 class="left fleft">Special Occasions</h1>
+            <div id="sp-img3" class="overflow"><img src="img/sp-img3.jpg">
+            </div>
+            <div id="sp2-left">
+                <div id="sp-img4" class="overflow"><img src="img/sp-img4.jpg">
+                </div>
+                <div id="sp-img5" class="overflow"><img src="img/sp-img5.jpg">
+                </div>
+                <div id="sp-img6" class="overflow"><img src="img/sp-img6.jpg">
+                </div>
+            </div>
+            
+            <div id="sp2-right">
+                <div id="blog2">
+                    <div id="blog-in2">
+                        <span>
+                            A one night Wedding Package <br>
+                            of THB 300,000.- <br>
+                             <div class="white-line"></div>
+                            Inclusive of ;<br>
+                            -   Superior and Deluxe Villas <br>
+                                with 3 persons sharing per room<br>
+                            -   One Two-bedroom Villa <br>
+                                for 5 persons sharing in room<br>
+                            -   Breakfast for all guests in-house <br>
+                                on the following day <br>
+                                (counting at 50 persons)<br>
+                            -   A buffet for 80 to 100 guests <br>
+                                inside dining hall   <br>   
+                                with soft drinks for 3 hours.<br>
+                            -   A flower decoration as per La Provence <br>
+                                (to be discussed with guests directly)<br>
+                            -   A complimentary stay for Bride & Groom <br>
+                                one night prior to the event <br>
+                                 <div class="white-line"></div>
+                                 Please contact us for more information <br>
+                                at : cm@laprovenceSP.com
+                             </span>
+
+                    </div>
+                </div>
+            </div>
+                <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                <div class="clearfix"></div>
+             <h1 class="left fleft">Pre-wedding photo shooting package</h1><h2>*Monday to Friday only*</h2>
+            <div id="pw-left">
+                <div id="blog3">
+                    <div id="blog-in3">
+                        <span>PACKAGE I  at 4,000 Baht <br><br>
+                        A special price when guest buy one<br>
+                        night accommodation through hotel<br>
+                        at normal rate.  Please call hotel <br>
+                        directly to request.<br><br>
+                        PACKAGE II  at 7,000 Baht<br> <br>
+                        One day package<br> 
+                        with no accommodation<br>
+                         <div class="white-line"></div>
+                        Inclusive of;<br>
+                        -   Photo shooting time starting <br>
+                            from 9 am to 7 pm<br> 
+                            (outdoor area only)<br>
+                        -   Photo shooting at The Pool is<br>           
+                        allow till 4 pm. Only.<br> 
+                        -   Indoor photo shooting at Bride<br> 
+                             & Groom’s room.<br>
+                        Remark;<br>
+                        -   Pets are not allow in the property.<br>
+                        -   No outside food allow in hotel<br> 
+                            and restaurant area.<br>
+                        -   Props, chairs and hotel belongings<br>      in rooms, restaurant and all            outdoor area can not be move from       original place.  
+                        -   A cash deposit of BHT 2,000 is to <br>      be place upon check-in to 
+                            guaranteed for any damage in hotel<br>      property. Hotel will return if no       damage occur upon check-out.
+
+                            </span>
+                    </div>
+                </div>
+            </div>
+
+            <div id="pw-right">
+                <div id="pw-img1" class="overflow"><img src="img/pw-img1.jpg">
+                </div>
+                <div id="pw-img2" class="overflow"><img src="img/pw-img2.jpg">
+                </div>
+                <div id="pw-img3" class="overflow"><img src="img/pw-img3.jpg">
+                </div>
+                <div id="pw-img4" class="overflow"><img src="img/pw-img4.jpg">
+                </div>
+            </div>
+
+             <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flowerline.jpg">
+                </div>
+                 <div class="clearfix"></div>
+            <div id="as-left">
+             <h1 class="left">Activities <br>
+                & Services</h1>
+                <h2>1.  Half-day trip to Huay Kok Moo 
+    plus Kayak<br><br>
+2.  ……..</h2>
+                </div>
+            <div id="as-right">
+               <div id="as-img1" class="overflow"><img src="img/as-img1.jpg">
+            </div>
+             <div id="as-img2" class="overflow"><img src="img/as-img2.jpg">
+            </div>
+        </div>
+
+                <div class="clearfix"></div>
+                 <div class="linelarge">
+                    <img src="images/flower-line3.jpg">
+                </div>
+
+
         <div class="gotop">
 	        <img src="images/gotop.png">
         </div>
         
-        
+        </div>
+      <!--inner-wrapper-->
         </div>
        
         
