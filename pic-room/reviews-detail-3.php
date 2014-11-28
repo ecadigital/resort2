664413ad2a840278fdfdcbc2d1150014c033135b
GreanMaster <Mastergrean@gmail.com>
2014-11-28 14:29:22 +0700

    update from host

---
+++ pic-room/reviews-detail-3.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=1167">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/layout.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+        
+        <?php include("inc-topmenu.php");?>
+        
+        
+        <div id="inner-wrapper">
+            <hgroup>
+	           <h1 class="left">Reviews</h1>
+               <h2>คุณมาเรีย ณ.ไกลบ้าน</h2>
+               <h3>30 March, 2014</h3>
+            </hgroup>
+
+            <div class="linelarge">
+                <img src="images/flower-line7.jpg">
+            </div>
+            <div class="clearfix"></div>
+            
+            <div class="container">
+                <div style="text-align: center;">
+                    <img src="img/rv-img3.jpg" alt="">
+                </div>
+                <p>คุณมาเรีย ณ.ไกลบ้าน<br> KTC TBA 2014-2015 : La Provence<br> รีสอร์ทสวยบรรยากาศโพรว๊องซ์ ที่สวนผึ้ง<br> <a href="http://www.bloggang.com/viewdiary.php?id=jbunny&month=07-2014&date=25&group=54&gblog=18">http://www.bloggang.com/viewdiary.php?id=jbunny&month=07-2014&date=25&group=54&gblog=18</a></p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam eius unde ipsum veniam enim animi ullam blanditiis aspernatur dolor soluta. Aut eligendi, neque repellendus, sit assumenda laboriosam mollitia, maiores dolor sequi quasi asperiores iste? Illum cupiditate ut voluptates vitae quia dicta nobis, nihil, dolores animi, obcaecati, autem voluptas rerum tempore.</p>
+                <p>Similique dicta obcaecati ab ipsum, beatae deleniti quasi, placeat fugiat earum, consequuntur velit dolor odio ratione magni error est nobis quam animi illo praesentium eveniet repudiandae possimus iure. Ducimus a quia itaque id maxime obcaecati voluptatibus molestias consequatur, perspiciatis error. Quasi eos id voluptas ea architecto dignissimos delectus sequi, nisi, rerum amet molestiae assumenda totam veritatis hic tempora quos repellendus, perferendis, porro error ut reiciendis. Hic quidem magnam nostrum voluptatum molestiae vero, excepturi non doloribus et nemo soluta aspernatur placeat similique natus quos nihil! Nam nemo accusantium, distinctio, odio pariatur inventore perspiciatis.</p>
+                <div class="shareit">
+                    <img src="images/share.png" alt="">
+                </div>
+            </div>
+            <div class="backto">
+           <a href="reviews.php"><img src="images/backto.png"></a> 
+        </div>
+        
+        </div><!--inner-wrapper-->
+       
+        
+        
+        
+<?php include('inc-footer.php');?> 
+        
+        
+        <script>
+	        $(document).ready(function(){
+		        $('#reviewa').addClass('active');
+		        $(this).areaAnimate0();
+	        });
+        </script>
+    </body>
+</html>
